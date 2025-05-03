3b9c3e3889ba2baf82aea955bd1c0dd1326bcc55
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2025-05-03 22:53:41 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,  // ⚠️ Temporarily disable ESLint errors during build
+  },
+};
+
+export default nextConfig;
